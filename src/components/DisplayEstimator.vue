5a7583c9bc7370b14952261afb47f08fa2e08453
tonymj76 <demeyortj@gmail.com>
2020-04-14 00:15:19 +0100

    fin

--- src/components/DisplayEstimator.vue
+++ src/components/DisplayEstimator.vue
@@ -2,20 +2,136 @@
   <v-row justify="center">
     <v-dialog
       v-model="dialog"
-      persistent
-      max-width="290"
-      fullscreen
+      width="80%"
       hide-overlay
       transition="dialog-bottom-transition"
     >
       <v-card>
-        <v-card-title class="headline">Use Google's location service?</v-card-title>
-        <v-card-text>{{estimateData | json 2}}</v-card-text>
-        <v-card-actions>
+        <v-toolbar dark color="primary">
+          <v-btn icon dark @click.stop="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>{{title}}</v-toolbar-title>
           <v-spacer></v-spacer>
-          <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
-          <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
-        </v-card-actions>
+          <v-toolbar-items>
+            <v-btn dark text @click.stop="dialog = false">close</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <v-row>
+          <v-col sm="12" md="6">
+            <v-list three-line subheader>
+              <v-subheader
+              class="text-uppercase font-weight-bold text-center">Impact</v-subheader>
+              <v-divider></v-divider>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    currently infected</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.impact.currentlyInfected}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>infections By Requested Time</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.impact.infectionsByRequestedTime}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Hospital Beds By Requested Time</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.impact.hospitalBedsByRequestedTime}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Cases For ICU By Requested Time</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.impact.casesForICUByRequestedTime}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Cases For Ventilators By Requested Time</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.impact.casesForVentilatorsByRequestedTime}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Dollars In Flight</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.impact.dollarsInFlight}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-col>
+
+          <v-col sm="12" md="6">
+            <v-list three-line subheader>
+              <v-subheader
+              class="text-uppercase font-weight-bold text-center">Severe Impact</v-subheader>
+              <v-divider></v-divider>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    currently infected</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.severeImpact.currentlyInfected}}
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>infections By Requested Time</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.severeImpact.infectionsByRequestedTime}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Hospital Beds By Requested Time</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.severeImpact.hospitalBedsByRequestedTime}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Cases For ICU By Requested Time</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.severeImpact.casesForICUByRequestedTime}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Cases For Ventilators By Requested Time</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.severeImpact.casesForVentilatorsByRequestedTime}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Dollars In Flight</v-list-item-title>
+                  <v-list-item-subtitle>
+                    {{estimateData.severeImpact.dollarsInFlight}}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-col>
+        </v-row>
       </v-card>
     </v-dialog>
   </v-row>
@@ -31,7 +147,8 @@ export default {
   },
   data() {
     return {
-      dialog: this.show
+      dialog: this.show,
+      title: 'Build For SDG'
     };
   }
 };
